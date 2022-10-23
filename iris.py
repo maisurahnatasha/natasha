442f12469b980010a1208318f2933f4a04b749d9
maisurahnatasha <115910527+maisurahnatasha@users.noreply.github.com>
2022-10-23 11:39:29 +0800

    Update iris.py

--- iris.py
+++ iris.py
@@ -1,4 +1,4 @@
-
+import streamlit as st 
 import pandas as pd
 from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
@@ -13,8 +13,8 @@ y_iris = iris['species']
 
 xtrain, xtest, ytrain, ytest = train_test_split(X_iris, y_iris, random_state = 0)
 clf = SVC(kernel='rbf', C=1).fit(xtrain, ytrain)
-print('Iris dataset')
-print('Accuracy of RBF SVC classifier on training set: {:.2f}'
+write.st('Iris dataset')
+write.st('Accuracy of RBF SVC classifier on training set: {:.2f}'
      .format(clf.score(xtrain, ytrain)))
-print('Accuracy of RBF SVC classifier on test set: {:.2f}'
+write.st('Accuracy of RBF SVC classifier on test set: {:.2f}'
      .format(clf.score(xtest, ytest)))
